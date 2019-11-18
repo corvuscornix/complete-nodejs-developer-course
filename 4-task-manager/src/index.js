5071983893de4b265ac67618f49c9b1869bc6068
Antero Tanskanen <android.tanskanen@gmail.com>
2019-11-18 20:04:21 +0200

    refactored to use async/await, implemented rest of the API endpoints

--- 4-task-manager/src/index.js
+++ 4-task-manager/src/index.js
@@ -9,24 +9,154 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body);
 
-    user.save().then(() => {
+    try {
+        await user.save();
         res.status(201).send(user);
-    }).catch(e => {
+    } catch (e) {
         res.status(400).send(e);
-    })
+    }
 });
 
-app.post('/tasks', (req, res) => {
+app.get('/users', async (req, res) => {
+
+    try {
+        const users = await User.find({});
+        
+        res.send(users);
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+app.get('/users/:id', async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const user = await User.findById(_id);
+        if (!user) {
+            return res.status(404).send();   
+        }
+        res.send(user);
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({error: 'Invalid updates!'});
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+
+        if (!user) {
+            res.status(404).send({error: 'No user found for given id'});
+        }
+
+        res.send(user);
+
+    } catch (e) {
+        res.status(400).send();
+    }
+});
+
+app.delete('/users/:id', async (req, res) => {
+
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    } catch (e) {
+        res.status(500).send();
+    }
+
+});
+
+/* ---------- TASKS ---------- */
+
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body);
 
-    task.save().then(() => {
+    try {
+        await task.save();
         res.status(201).send(task);
-    }).catch(e => {
+    } catch (e) {
         res.status(400).send(e);
-    })
+    }
+});
+
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.send(tasks);
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+app.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const task = await  Task.findById(_id);
+        if (!task) {
+            return res.status(404).send();   
+        }
+        res.send(task);
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'completed'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({error: 'Invalid updates!'});
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        console.log(task)
+        if (!task) {
+            res.status(404).send({error: 'No task found for given id'});
+        }
+
+        res.send(task);
+
+    } catch (e) {
+        res.status(400).send();
+    }
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+
+        if (!task) {
+            return res.status(404).send();
+        }
+
+        res.send(task);
+    } catch (e) {
+        res.status(500).send();
+    }
+
 });
 
 
