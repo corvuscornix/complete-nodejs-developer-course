48ee63037d59d9ad716949eeb90c8e94bbbbfc39
Antero Tanskanen <android.tanskanen@gmail.com>
2019-11-24 12:45:59 +0200

    Finished chat application
    - rooms
    - sign in page
    - time stamps and formatting
    - users list
    - styles

--- 5-chat-app/public/js/chat.js
+++ 5-chat-app/public/js/chat.js
@@ -6,10 +6,62 @@ const sendLocationButton = document.getElementById('send-location');
 const messageForm = document.getElementById('message-form');
 const messages = document.getElementById('messages');
 const messageTemplate = document.getElementById('message-template').innerHTML;
+const locationTemplate = document.getElementById('location-template').innerHTML;
+const sideBarTemplate = document.getElementById('sidebar-template').innerHTML;
+
+// Options
+const {username, room} = Qs.parse(location.search, { ignoreQueryPrefix: true });
+
+const autoscroll = () => {
+    const lastMessage = messages.lastElementChild;
+
+    // Height of the last message
+    const lastMessageStyles = getComputedStyle(lastMessage);
+    const lastMessageMargin = parseInt(lastMessageStyles.marginBottom);
+    const lastMessageHeight = lastMessage.offsetHeight + lastMessageMargin;
+
+    // Visible height
+    const visibleHeight = messages.offsetHeight;
+
+    // Height of messages container
+    const containerHeight = messages.scrollHeight;
+
+    // How far have I scrolled?
+    const scrollOffset = messages.scrollTop + visibleHeight;
+
+    if (containerHeight - lastMessageHeight <= scrollOffset) {
+        messages.scrollTop = messages.scrollHeight;
+    }
+}
 
 socket.on('message', (message) => {
-    const html = Mustache.render(messageTemplate, {message});
+    console.log(message);
+    const html = Mustache.render(messageTemplate, {
+        username: message.username,
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    });
     messages.insertAdjacentHTML('beforeend', html);
+    autoscroll();
+});
+
+socket.on('locationMessage', (message) => {
+    const html = Mustache.render(locationTemplate, {
+        username: message.username,
+        url: message.url,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    });
+    messages.insertAdjacentHTML('beforeend', html);
+    autoscroll();
+});
+
+
+socket.on('roomData', ({ users, room }) => {
+    const html = Mustache.render(sideBarTemplate, {
+        users,
+        room
+    });
+    document.getElementById('sidebar').innerHTML = html;
 });
 
 function sendMessage () {
@@ -59,3 +111,10 @@ sendLocationButton.addEventListener('click', () => {
         });
     });
 })
+
+socket.emit('join', { username, room }, (error) => {
+    if (error) {
+        alert(error);
+        location.href = '/';
+    }
+});
--- 5-chat-app/src/index.js
+++ 5-chat-app/src/index.js
@@ -5,6 +5,8 @@ const Filter = require('bad-words');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+const { generateMessage, generateLocationMessage } = require('./utils/messages');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users');
 
 const port = process.env.PORT || 3000;
 
@@ -13,27 +15,59 @@ app.use(express.static('public'));
 let count = 0;
 
 io.on('connection', (socket) => {
-    console.log('new connection');
+
+    socket.on('join', ({ username, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, username, room });
+        
+        if (error) {
+            return callback(error);
+        }
+
+        socket.join(user.room);
+
+        socket.emit('message', generateMessage('Chat','Welcome'));
+        socket.broadcast.to(user.room).emit('message', generateMessage('Chat', `${user.username} has joined!`));
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        });
+
+        callback();
+    });
 
     socket.on('message', (message, callback) => {
         const filter = new Filter();
         if (filter.isProfane(message)) {
             return callback('Profanity is not allowed');
         }
-        io.emit('message', message);
+
+        const user = getUser(socket.id);
+
+        if (!user) {
+            return callback('You\' not logged in');
+        }
+
+        io.to(user.room).emit('message', generateMessage(user.username, message));
         callback();
     });
 
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+        const user = getUser(socket.id);
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`));
         callback();
     });
 
-    
-    socket.broadcast.emit('message', `A new user has joined`);
 
     socket.on('disconnect', () => {
-        io.emit('message', `A user has left`);
+        const user = removeUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('message', generateMessage('Chat', `${user.username} has left`));
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            });
+        }
     });
     
 });
