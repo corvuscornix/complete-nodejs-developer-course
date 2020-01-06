fb78fc38dbada0abaccf40fda23aac086e3099bc
Antero Tanskanen <android.tanskanen@gmail.com>
2020-01-06 14:28:52 +0200

    removed personal API keys from code

--- 2-weather-app/utils.js
+++ 2-weather-app/utils.js
@@ -1,52 +1,53 @@
-const request = require('request')
+const request = require('request');
 
 // String format polyfill
 if (!String.prototype.format) {
     String.prototype.format = function() {
-        var args = arguments
-        return this.replace(/{(\d+)}/g, function(match, number) { 
-        return typeof args[number] != 'undefined'
-            ? args[number]
-            : match
-        ;
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number) {
+            return typeof args[number] != 'undefined' ? args[number] : match;
         });
     };
 }
 
-const weatherUrlTemplate = 'https://api.darksky.net/forecast/7fc4c61c3e9666edf36a3acdcce6b975/{0},{1}?units=si&lang=en&limit=1'
-const geocodingUrlTemplate = 'https://api.mapbox.com/geocoding/v5/mapbox.places/{0}.json?access_token=pk.eyJ1IjoiYW50ZXJvdGFuIiwiYSI6ImNrMGh0cjBxYzAwNHMzaW1jdmVkYWxxbmsifQ.TRRP_pziYVCaL8xhcH6uXg';
+const weatherUrlTemplate = `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/{0},{1}?units=si&lang=en&limit=1`;
+const geocodingUrlTemplate = `https://api.mapbox.com/geocoding/v5/mapbox.places/{0}.json?access_token=${process.env.MAPBOX_API_KEY}`;
 
 function fetchCoordinatesForLocationName(location, callback, errorCallback) {
-    const url = geocodingUrlTemplate.format(location)
-    request.get(
-        {url, json: true},
-        (error, { body }) => {
-            if (error) {
-                errorCallback({error: 'Unable to connect to location service!'})
-            } else if (!body.features.length) {
-                errorCallback({error: `Didn't find location for '${body.query.join(' ')}'`})
-            } else {
-                const location = body.features[0];
-                callback(location);
-            }
-            
-        })
+    const url = geocodingUrlTemplate.format(location);
+    request.get({ url, json: true }, (error, { body }) => {
+        if (error) {
+            errorCallback({ error: 'Unable to connect to location service!' });
+        } else if (!body.features.length) {
+            errorCallback({
+                error: `Didn't find location for '${body.query.join(' ')}'`
+            });
+        } else {
+            const location = body.features[0];
+            callback(location);
+        }
+    });
 }
 
-function fetchWeatherDataForCoordinates(longitude, latitude, callback, errorCallback) {
-    const url = weatherUrlTemplate.format(latitude, longitude)
-    request.get({url, json: true}, (error, { body }) => {
+function fetchWeatherDataForCoordinates(
+    longitude,
+    latitude,
+    callback,
+    errorCallback
+) {
+    const url = weatherUrlTemplate.format(latitude, longitude);
+    request.get({ url, json: true }, (error, { body }) => {
         if (error) {
-            errorCallback({error: 'Unable to connect to weather service!'})
+            errorCallback({ error: 'Unable to connect to weather service!' });
         } else if (body.error) {
-            errorCallback({error: `${body.error} (${body.code})`})
+            errorCallback({ error: `${body.error} (${body.code})` });
         } else {
-            callback(body)
+            callback(body);
         }
-    })
+    });
 }
 
 module.exports = {
     fetchCoordinatesForLocationName,
     fetchWeatherDataForCoordinates
-}
+};
--- 3-web-server/src/utils.js
+++ 3-web-server/src/utils.js
@@ -1,68 +1,77 @@
-const request = require('request')
+const request = require('request');
 
 // String format polyfill
 if (!String.prototype.format) {
     String.prototype.format = function() {
-        var args = arguments
-        return this.replace(/{(\d+)}/g, function(match, number) { 
-        return typeof args[number] != 'undefined'
-            ? args[number]
-            : match
-        ;
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number) {
+            return typeof args[number] != 'undefined' ? args[number] : match;
         });
     };
 }
 
-const weatherUrlTemplate = 'https://api.darksky.net/forecast/7fc4c61c3e9666edf36a3acdcce6b975/{0},{1}?units=si&lang=en&limit=1'
-const geocodingUrlTemplate = 'https://api.mapbox.com/geocoding/v5/mapbox.places/{0}.json?access_token=pk.eyJ1IjoiYW50ZXJvdGFuIiwiYSI6ImNrMGh0cjBxYzAwNHMzaW1jdmVkYWxxbmsifQ.TRRP_pziYVCaL8xhcH6uXg';
+const weatherUrlTemplate = `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/{0},{1}?units=si&lang=en&limit=1`;
+const geocodingUrlTemplate = `https://api.mapbox.com/geocoding/v5/mapbox.places/{0}.json?access_token=${process.env.MAPBOX_API_KEY}`;
+
+console.log(process.env.DARKSKY_API_KEY, process.env.MAPBOX_API_KEY);
 
 function fetchCoordinatesForLocationName(location, callback, errorCallback) {
-    const url = geocodingUrlTemplate.format(location)
-    request.get(
-        {url, json: true},
-        (error, { body }) => {
-            if (error) {
-                errorCallback({error: 'Unable to connect to location service!'})
-            } else if (!body.features.length) {
-                errorCallback({error: `Didn't find location for '${body.query.join(' ')}'`})
-            } else {
-                const location = body.features[0];
-                callback(location);
-            }
-            
-        })
-}
-
-function fetchWeatherDataForCoordinates(longitude, latitude, callback, errorCallback) {
-    const url = weatherUrlTemplate.format(latitude, longitude)
-    request.get({url, json: true}, (error, { body }) => {
+    const url = geocodingUrlTemplate.format(location);
+    request.get({ url, json: true }, (error, { body }) => {
         if (error) {
-            errorCallback({error: 'Unable to connect to weather service!'})
-        } else if (body.error) {
-            errorCallback({error: `${body.error} (${body.code})`})
+            errorCallback({ error: 'Unable to connect to location service!' });
+        } else if (!body.features.length) {
+            errorCallback({
+                error: `Didn't find location for '${body.query.join(' ')}'`
+            });
         } else {
-            callback(body)
+            const location = body.features[0];
+            callback(location);
         }
-    })
+    });
 }
 
-function fetchWeatherDataForLocation (locationName, callback) {
-    fetchCoordinatesForLocationName(locationName,
-        (locationData) => {
-            const {center} = locationData
-            fetchWeatherDataForCoordinates(center[0], center[1], (weatherData) => {
-                callback(undefined, {
-                    locationData,
-                    weatherData
-                })
-            }, (error) => callback(error))
+function fetchWeatherDataForCoordinates(
+    longitude,
+    latitude,
+    callback,
+    errorCallback
+) {
+    const url = weatherUrlTemplate.format(latitude, longitude);
+    request.get({ url, json: true }, (error, { body }) => {
+        if (error) {
+            errorCallback({ error: 'Unable to connect to weather service!' });
+        } else if (body.error) {
+            errorCallback({ error: `${body.error} (${body.code})` });
+        } else {
+            callback(body);
+        }
+    });
+}
+
+function fetchWeatherDataForLocation(locationName, callback) {
+    fetchCoordinatesForLocationName(
+        locationName,
+        locationData => {
+            const { center } = locationData;
+            fetchWeatherDataForCoordinates(
+                center[0],
+                center[1],
+                weatherData => {
+                    callback(undefined, {
+                        locationData,
+                        weatherData
+                    });
+                },
+                error => callback(error)
+            );
         },
-        (error) => callback(error)
-    )
+        error => callback(error)
+    );
 }
 
 module.exports = {
     fetchCoordinatesForLocationName,
     fetchWeatherDataForCoordinates,
     fetchWeatherDataForLocation
-}
+};
